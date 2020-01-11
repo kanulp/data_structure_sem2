903c0a8f51ec5ecc59667f3a22bbc23d0aff5e3d
Karan Gajjar <kanu@Karans-MacBook-Pro.local>
2020-01-11 00:18:06 -0500

    first commit

---
+++ W1Lab2-Gajjar/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace W1Lab2_Gajjar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of QuickPick numbers to generate:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] arr = new int[n,6];
+            int[] temp = new int[6];
+            int k=0;
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                
+                for (int j = 0; j < 6; j++)
+                {
+
+                    int x=getNumber(temp);
+                    arr[i,j] = x;
+                    
+                }
+                
+                k=0;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    Console.Write(arr[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+       
+       static int[] getNumber(){
+           int[] arr = new int[6];
+           for (int i = 0; i < arr.Length; i++)
+           {
+               int x = new Random().Next(1,49);
+               if(arr.Contains(x))
+                i--;
+           }
+
+           return arr;
+       }
+
+
+    }
+}
---
+++ W2Lab1-Gajjar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace W2Lab1_Gajjar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[7];
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i]=random.Next(1,49);
+            }
+            Array.Sort(arr);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
